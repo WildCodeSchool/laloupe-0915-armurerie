bb0b021f025c92b99e4e4a67170911202daae10d
alexandre cuillerier <alexandre.cuillerier@gmail.com>
2015-10-19 15:59:36 +0200

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+classes = Classe.create(
+	[	{ name: 'Toutes'},
+		{ name: 'Paladin'},
+		{ name: 'Guerrier'},
+		{ name: 'Magicien'},
+		{ name: 'Assassin'}
+	]
+)
+
+users = User.create(
+	[	
+		{ nickname: 'Syyrius', level: 100, loot: 1000000},
+		{ nickname: 'Zongo', level: 2, loot: 9},
+		{ nickname: 'Gandalf', level: 1000, loot: 0},
+		{ nickname: 'Tsoin', level: 6, loot: 160}
+	]
+)
+
+equipments = Equipment.create(
+	[
+		{name: 'Excalibur', item: 'Sword', location: '2Hands', price:1000000, level:99, bonus: 'Agility', material: 'Steel'},
+		{name: 'YouShallNotPass', item: 'Shield', location: 'Arm', price:300, level:66, bonus: 'Protection', material: 'Wood and Iron'},
+		{name: 'IceStick', item: 'Stick', location: 'Hand', price:44, level:12, bonus: '', material: 'Wood'},
+		{name: '!! TSCHAK !!', item: 'Axe', location: '2Hands', price:888, level:100, bonus: 'Bleed', material: 'Dwarf Steel'},
+		{name: 'Fireman', item: 'Helmet', location: 'Head', price:76, level:29, bonus: '', material: 'Iron'},
+		{name: 'Shadow', item: 'Cape', location: 'Back', price:104, level:77, bonus: 'Invisibility', material: 'Silk'}
+	]
+)
