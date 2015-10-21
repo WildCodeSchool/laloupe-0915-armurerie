21e8f8ce63da25c1a8c6b997c7e77128d4c7cfb3
julienFR88 <julien.vallar@gmail.com>
2015-10-21 17:12:40 +0200

    devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	has_many :baskets
 
 	validates_presence_of :nickname, message: "Le nom ne doit pas être vide"
--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,8 @@ module Armurerie
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    # Heroku with Devise
+    config.assets.initialize_on_precompile = false
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
