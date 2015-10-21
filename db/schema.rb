e1139d7e9a5b2cbe47de305d5c3203f04a93183f
julienFR88 <julien.vallar@gmail.com>
2015-10-21 10:25:51 +0200

    Merge branch 'jv_notice'
    Conflicts:
    	app/models/equipment.rb
    	app/models/user.rb

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20151020094202) do
 
   add_index "baskets", ["user_id"], name: "index_baskets_on_user_id"
 
+  create_table "class_names", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "classes", force: :cascade do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
