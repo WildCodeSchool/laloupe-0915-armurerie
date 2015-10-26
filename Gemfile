5ec9066a8b513157858c25682f537e443f64de95
julienFR88 <julien.vallar@gmail.com>
2015-10-26 10:55:46 +0100

    adds seeds

--- Gemfile
+++ Gemfile
@@ -36,7 +36,6 @@ gem "haml-rails", "~> 0.9"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
   gem 'pry'
 end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,17 @@ classes = Classe.create(
 		{ name: 'Paladin'},
 		{ name: 'Guerrier'},
 		{ name: 'Magicien'},
-		{ name: 'Assassin'}
+		{ name: 'Assassin'},
+		{ name: 'Cambrioleur'},
+		{ name: 'Sentinelle'},
+		{ name: 'Gardien des Runes'},
+		{ name: 'Capitaine'},
+		{ name: 'Champion'},
+		{ name: 'Chasseur'},
+		{ name: 'Gardien'},
+		{ name: 'Maître du savoir'},
+		{ name: 'Ménestrel'}
+
 	]
 )
 
@@ -20,7 +30,18 @@ users = User.create(
 		{ nickname: 'Syyrius', level: 100, loot: 1000000},
 		{ nickname: 'Zongo', level: 2, loot: 9},
 		{ nickname: 'Gandalf', level: 1000, loot: 0},
-		{ nickname: 'Tsoin', level: 6, loot: 160}
+		{ nickname: 'Tsoin', level: 6, loot: 160},
+		{ nickname: 'Frodon', level: 3, loot: 150},
+		{ nickname: 'Saroumane', level: 100000, loot: 1000000},
+		{ nickname: 'Sauron', level: 1000000, loot: 0},
+		{ nickname: 'Gimli', level: 150, loot: 16000},
+		{ nickname: 'Bilbo', level: 5, loot: 160},
+		{ nickname: 'Smeagol', level: 300, loot: 1000},
+		{ nickname: 'Smaug', level:30000, loot: 250000000},
+		{ nickname: 'Thorin', level: 1500, loot: 160000},
+		{ nickname: 'Legolas', level: 300, loot: 160000},
+		{ nickname: 'Boromir', level: 300, loot: 120000},
+		{ nickname: 'Théoden', level: 3000, loot: 1600}
 	]
 )
 
